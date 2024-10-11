ab8fede07d7f48da01ed05def9a0b8492450be56
lokesh.viswanathan <lokesh.viswanathan@worldline.com>
2024-10-11 15:01:58 +0530

    first comit

---
+++ src/main/java/org/mobiletesting/Main.java
@@ -0,0 +1,9 @@
+package org.mobiletesting;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6; i++) {
+            System.out.println("Hello world! this is for jenkis");
+        }
+    }
+}
