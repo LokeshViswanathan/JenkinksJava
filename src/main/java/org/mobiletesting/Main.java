09058f82c301b91d3657a426d457eba08c7101d4
LokeshViswanathan <163581086+LokeshViswanathan@users.noreply.github.com>
2024-10-13 05:28:47 -0700

    Update Main.java
    commit

--- src/main/java/org/mobiletesting/Main.java
+++ src/main/java/org/mobiletesting/Main.java
@@ -3,7 +3,7 @@ package org.mobiletesting;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 6; i++) {
-            System.out.println("Hello world! this is for jenkis");
+            System.out.println("Hello world! this is for jenkins");
         }
         System.out.println("first commit afterr jenkins");
       
